12c910d3043d59e418aff92286adff476bb49218
Igor Medeiros <igor.medeiros@gmail.com>
2022-12-10 17:33:06 -0300

    first commit: project structure created

---
+++ src/main/java/br/com/igormedeiros/com/br/bockstoremanager/BockstoremanagerApplication.java
@@ -0,0 +1,13 @@
+package br.com.igormedeiros.com.br.bockstoremanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BockstoremanagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BockstoremanagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/igormedeiros/com/br/bockstoremanager/BockstoremanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.igormedeiros.com.br.bockstoremanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BockstoremanagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
